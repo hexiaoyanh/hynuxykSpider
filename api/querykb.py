78fab826ca95e76473c27251042ba63a79f3bb14
Mika <heiaoyang@foxmail.com>
2019-10-25 20:33:56 +0800

    fix bugs

--- api/querykb.py
+++ api/querykb.py
@@ -21,10 +21,20 @@ class querykb(login):
                 week = []
                 for k in range(1, 4):
                     strs = "{0}-{1}-{2}".format(i, j, k)
-                    temp = {strs: soup.find(id=strs).text.replace('\xa0', '')}
+                    if k != 2:
+                        temp = {strs: soup.find(id=strs).text.replace('\xa0', '')}
+                    else:
+                        s = str(soup.find(id=strs))
+                        s = s.replace("<div id=\"{0}\" style=\"display: none;\">".format(strs), '')
+                        s = s.replace("</div>", '')
+                        s = s.replace("<br/>", ' ')
+                        s = s.replace("<nobr>", " ")
+                        s = s.replace("</nobr>", ' ')
+                        s = s.replace('\xa0', '')
+                        temp = {strs: s}
                     week.append(temp)
                 data.append(week)
-        return json.dumps(data,ensure_ascii=False)
+        return json.dumps(data, ensure_ascii=False)
 
     def queryallkb(self, date, week):
         self.url = "http://59.51.24.46/hysf/tkglAction.do?method=goListKbByXs&istsxx=no&xnxqh={0}&zc={1}&xs0101".format(
